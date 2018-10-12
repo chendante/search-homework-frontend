6f5807bd815a32b2b9ae51d5de4697ac1e4490bf
LiuZichen <danteliuzichen@163.com>
2018-10-12 13:53:59 +0800

    首页

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import index from '@/views/index'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: '首页',
+      component: index
     }
   ]
 })
---
+++ src/views/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <el-row>
+        <el-col :span="24">
+            歌名：
+        </el-col>
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+    </el-row>
+</template>
+<script>
+
+export default {
+  data(){
+    return {
+      input: ''
+    }
+  }
+}
+
+</script>
